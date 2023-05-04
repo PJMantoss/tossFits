59de7c4938afefb726b1e3cff1589a45f8084c4f
PJMantoss <joelptoss@gmail.com>
2023-05-04 19:42:10 +0100

    update Nav component

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,7 +3,12 @@ import Link from 'next/link';
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+        <Link href='/products'>Products</Link>
+        <Link href='/sell'>sell</Link>
+        <Link href='/orders'>Orders</Link>
+        <Link href='/account'>Account</Link>
+    </nav>
   )
 }
 
