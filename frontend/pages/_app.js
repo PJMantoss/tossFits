1aae2a95b4d1fa83e83f05ceb1c8f2c5343177f4
PJMantoss <joelptoss@gmail.com>
2023-05-07 16:52:34 +0100

    update Global styling in Page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,11 @@
 import Page from "../components/Page";
 import NProgress from "nprogress";
 import Router from 'next/router';
+import '../components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
-
-import 'nprogress/nprogress.css'
 
 function theApp({ Component, pageProps }) {
   return (
