65ceb7f6d6d048ce32de3351863d699c61d735af
PJMantoss <joelptoss@gmail.com>
2023-05-07 17:03:03 +0100

    update Document

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,8 @@ import { ServerStyleSheet } from 'styled-components';
 export class MyDocument extends Document {
   static getInitialProps({ renderPage }){
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styeTags = sheet.getStyleElement();
   }
 
   render() {
