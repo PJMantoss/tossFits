8044ce8010be882ccf8b509d220a2dc62fb26786
PJMantoss <joelptoss@gmail.com>
2023-05-08 16:53:42 +0100

    update keystone file in backend dir

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,3 +1,8 @@
 import 'dotenv/config';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-toss-fits';
+
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 365, // How long a user should stay signed-in
+    secret: 
+}
