e5b7aae21c33d48eb521654d9ea05e3c52401844
PJMantoss <joelptoss@gmail.com>
2023-05-08 18:46:34 +0100

    update keystone file in backend dir

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -2,7 +2,7 @@ import { config, createSchema } from '@keystone-next/keystone/schema';
 import { User } from './schemas/User';
 import 'dotenv/config';
 
-const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-toss-fits';
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
     maxAge: 60 * 60 * 24 * 365, // How long a user should stay signed-in
