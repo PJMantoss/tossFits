d3634292ae3900084ae454abcfa5ff4ffae6f9c0
PJMantoss <joelptoss@gmail.com>
2023-05-11 10:00:32 +0100

    update keystone file in backend dir

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -40,7 +40,9 @@ export default withAuth(config({
     }),
     ui: {
         // change this for roles
-        isAccessAllowed: () => true,
+        isAccessAllowed: ({ session }) => {
+            return session?.data
+        },
     },
     session: withItemData(statelessSessions(sessionConfig), {
         User: `id`,
