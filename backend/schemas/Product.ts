879fa4eb18bc5e113918fe9acdf6c659b0199a0b
PJMantoss <joelptoss@gmail.com>
2023-05-11 18:07:09 +0100

    update Product schema

--- backend/schemas/Product.ts
+++ backend/schemas/Product.ts
@@ -1,6 +1,5 @@
-import { text } from '@keystone-next/fields';
+import { text, select } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
-import { select } from '@keystone-next/fields';
 
 export const Product = list({
     fields: {
