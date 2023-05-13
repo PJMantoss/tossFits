a01ee12cbd7b356fba36de89152bd5bd704b2cd1
PJMantoss <joelptoss@gmail.com>
2023-05-13 12:51:56 +0100

    update ProductImage schema

--- backend/schemas/ProductImage.ts
+++ backend/schemas/ProductImage.ts
@@ -1,7 +1,13 @@
+import { cloudinaryImage } from "@keystone-next/cloudinary";
+import { text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 
 export const ProductImage = list({
     fields: {
-        
-    }
+        image: cloudinaryImage({
+            cloudinary,
+            label: 'Source',
+        }),
+        altText: text(),
+    },
 });
