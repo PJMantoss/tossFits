5a43b7174d6b1fe39697f00ab6d4550b55c1fdff
PJMantoss <joelptoss@gmail.com>
2023-05-13 12:57:27 +0100

    update ProductImage schema

--- backend/schemas/ProductImage.ts
+++ backend/schemas/ProductImage.ts
@@ -5,7 +5,8 @@ import { list } from "@keystone-next/keystone/schema";
 export const cloudinary = {
     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
     apiKey: process.env.CLOUDINARY_KEY,
-    apiSecret: process.env.CLOUDINARY_SECRET
+    apiSecret: process.env.CLOUDINARY_SECRET,
+    folder: 'tossfits'
 }
 
 export const ProductImage = list({
