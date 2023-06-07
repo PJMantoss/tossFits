0fd04b5ee9cae6166e74d5e6b825959a92620849
PJMantoss <joelptoss@gmail.com>
2023-06-07 16:45:19 +0100

    update FormatMoney utility

--- frontend/lib/formatMoney.js
+++ frontend/lib/formatMoney.js
@@ -7,5 +7,5 @@ export default function FormatMoney(amount=0){
 
     const formatter = Intl.NumberFormat('en-US', options);
 
-    return formatter.format(amount);
+    return formatter.format(amount/100);
 }
