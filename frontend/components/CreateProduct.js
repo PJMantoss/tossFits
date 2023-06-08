cb62250db9c3b63d7412b06e78fc1e176b5586ee
PJMantoss <joelptoss@gmail.com>
2023-06-08 10:50:19 +0100

    add CreateProduct component

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CreateProduct = () => {
+    return (
+        <form>
+            <label htmlFor="">
+                <input 
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder=""
+                />
+            </label>
+        </form>
+    );
+}
+
+export default CreateProduct;
