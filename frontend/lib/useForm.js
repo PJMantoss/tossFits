e80da528587d0402fa2303590ef812895d281b22
PJMantoss <joelptoss@gmail.com>
2023-06-08 11:16:35 +0100

    update useForm utility

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -1,5 +1,8 @@
+import { useState } from 'react';
+
 function useForm(initial = {}) {
     // create a state object for input
+    const [state, setstate] = useState(initialState);
 }
 
 export default useForm;
