14208d304190b3edb0f64d0960d87dd223dc2aa3
PJMantoss <joelptoss@gmail.com>
2023-06-10 20:10:51 +0100

    update useForm utility

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -3,15 +3,17 @@ import { useState } from 'react';
 function useForm(initial = {}) {
     // create a state object for input
     const [state, setstate] = useState(initial);
-}
 
-function handleChange(e){
-    const {name, value} = e.target;
-    setInputs({
-        // Copy existing state
-        ...inputs,
-        [name]: value
-    })
+    function handleChange(e){
+        const {name, value} = e.target;
+        setInputs({
+            // Copy existing state
+            ...inputs,
+            [name]: value
+        })
+    }
+
+    // Return what we want to surface from this custom hook
 }
 
 export default useForm;
