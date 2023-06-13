f5abb3a1890e4816099886f08795fabca6394477
PJMantoss <joelptoss@gmail.com>
2023-06-13 17:20:43 +0100

    update CreateProduct

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2e3cdce0486f8df09ff1"; }
+/******/ 		__webpack_require__.h = function() { return "ea66f2e75ad6d1bc8df7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -11,40 +11,39 @@ const CreateProduct = () => {
     return (
         <Form>
             <fieldset>
-            <label htmlFor="image">
-                Image
-                <input 
-                    id="image"
-                    name="image"
-                    type="file"
-                    onChange={handleChange}
-                />
-            </label>
+                <label htmlFor="image">
+                    Image
+                    <input 
+                        id="image"
+                        name="image"
+                        type="file"
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="name">
+                    Name
+                    <input 
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Name"
+                        value={inputs.name}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="price">
+                    Price
+                    <input 
+                        id="price"
+                        name="price"
+                        type="number"
+                        placeholder="Price"
+                        value={inputs.price}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button type="submit" onClick={resetForm}>Reset Form</button>
             </fieldset>
-            <label htmlFor="name">
-                Name
-                <input 
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder="Name"
-                    value={inputs.name}
-                    onChange={handleChange}
-                />
-            </label>
-            <label htmlFor="price">
-                Price
-                <input 
-                    id="price"
-                    name="price"
-                    type="number"
-                    placeholder="Price"
-                    value={inputs.price}
-                    onChange={handleChange}
-                />
-            </label>
-            <button type="button" onClick={resetForm}>Reset Form</button>
-            <button type="button" onClick={clearForm}>Clear Form</button>
         </Form>
     );
 }
