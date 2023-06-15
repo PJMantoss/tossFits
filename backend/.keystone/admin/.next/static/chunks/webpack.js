736aaa286e0cab6623d5a86bd5c9c8458626fc3f
PJMantoss <joelptoss@gmail.com>
2023-06-15 11:34:58 +0100

    update CreateProduct

--- backend/.keystone/admin/.next/static/chunks/webpack.js
+++ backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "529acddee70eed4d73a8";
+/******/ 	var hotCurrentHash = "d451596f50bd89604dc4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
