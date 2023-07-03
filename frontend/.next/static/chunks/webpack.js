0cafdbca5d9317af94de141616ac34d018ac3da0
PJMantoss <joelptoss@gmail.com>
2023-07-03 10:52:26 +0100

    update Single Product Page

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ac42a4b0ad0cd3fcc5e"; }
+/******/ 		__webpack_require__.h = function() { return "7e6fe0dff644de523e02"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
