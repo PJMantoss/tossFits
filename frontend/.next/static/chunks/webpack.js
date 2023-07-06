a53040a1e57f6ad3080f78857c2059b6a021c7b8
PJMantoss <joelptoss@gmail.com>
2023-07-06 11:32:57 +0100

    update DeleteProduct component

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03f478ae1020af535c59"; }
+/******/ 		__webpack_require__.h = function() { return "d77c083fc804c9872923"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
