016d78b047e1f0556f1d6b3a6c3d30432fd09899
PJMantoss <joelptoss@gmail.com>
2023-07-06 17:22:21 +0100

    update Pagination component

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import PaginationStyles from "./styles/PaginationStyles";
 
-export default function Pagination(){
+export default function Pagination({ page }){
     return (
         <PaginationStyles>
             <Head>
