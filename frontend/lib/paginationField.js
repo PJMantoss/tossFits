861841916c37bc7256d81d75dbad2ee6779ae5bf
PJMantoss <joelptoss@gmail.com>
2023-07-15 20:23:06 +0100

    update paginationField

--- frontend/lib/paginationField.js
+++ frontend/lib/paginationField.js
@@ -4,7 +4,7 @@ export default function paginationField(){
     return{
         keyArgs: false, // tells apollo to ignore and allow us handle caching
         // Ask the read function for the product items
-        read(existing = [], {args, cache, id}){
+        read(existing = [], {args, cache}){
             console.log({existing, args, cache});
 
             const { skip, first } = args;
