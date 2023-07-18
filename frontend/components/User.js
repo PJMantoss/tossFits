79fbb913c31dc0634c653d5af09c19ee249a445b
PJMantoss <joelptoss@gmail.com>
2023-07-18 12:16:14 +0100

    update User component

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -3,5 +3,5 @@ import { gql, useQuery } from '@apollo/client';
 const CURRENT_USER_QUERY = gql``;
 
 export default function useUser(){
-    const {} = useQuery();
+    const { data } = useQuery(CURRENT_USER_QUERY);
 }
