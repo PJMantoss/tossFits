53cd4e2dde890504f2c3162ed98b95b75477cfe6
PJMantoss <joelptoss@gmail.com>
2023-07-19 15:50:59 +0100

    update SignOut component

--- frontend/components/SignOut.js
+++ frontend/components/SignOut.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+const SIGNOUT_MUTATION = gql`
+    mutation {
+        endSession();
+    }
+`;
 
 function SignOut({ children }) {
+    const [ signout ] = useMutation(SIGNOUT_MUTATION);
   return (
     <button type="button">Sign Out</button>
   )
