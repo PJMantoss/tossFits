6c9e2bea76b3266c9d260b2e7a91743a0f5d4af2
PJMantoss <joelptoss@gmail.com>
2023-07-19 15:48:19 +0100

    update SignOut component

--- frontend/components/SignOut.js
+++ frontend/components/SignOut.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 function SignOut({ children }) {
   return (
-    <button type="button">SignOut</button>
+    <button type="button">Sign Out</button>
   )
 }
 
