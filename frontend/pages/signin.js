9604806bd51749c80ad04f5d21bcdbca16a7a244
PJMantoss <joelptoss@gmail.com>
2023-07-20 19:51:24 +0100

    update SignUp component

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -3,7 +3,11 @@ import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
 import styled from 'styled-components';
 
-const GridStyle = styled.div``;
+const GridStyle = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(18.75rem, 1fr));
+  grid-gap: 2rem;
+`;
 
 function signin() {
   return (
