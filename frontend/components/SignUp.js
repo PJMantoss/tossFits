ee9e222753762a6252b22db53ed69d5eac8c6d8b
PJMantoss <joelptoss@gmail.com>
2023-07-20 20:02:09 +0100

    update SignUp component

--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -7,7 +7,11 @@ import Error from './ErrorMessage';
 
 const SIGNUP_MUTATION = gql`
     mutation SIGNUP_MUTATION ($name: String! $email: String!, $password: String!){
-        createUser(data: {name: $name, email: $email, password: $password})
+        createUser(data: {name: $name, email: $email, password: $password}){
+            id
+            email
+            name
+        }
     }
 `;
 
