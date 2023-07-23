8c7d4935271dc393e1b042932c539f52decdfd6a
PJMantoss <joelptoss@gmail.com>
2023-07-23 11:14:15 +0100

    add mail.ts

---
+++ backend/lib/mail.ts
@@ -0,0 +1,8 @@
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'laurence.gottlieb@ethereal.email',
+        pass: 'VWpfhvnG5y3ydfwuqp'
+    }
+});
