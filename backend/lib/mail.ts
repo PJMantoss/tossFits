c16d8e8e8c8b964c243bfb73db64839efae11335
PJMantoss <joelptoss@gmail.com>
2023-07-29 18:28:10 +0100

    update .env

--- backend/lib/mail.ts
+++ backend/lib/mail.ts
@@ -21,7 +21,7 @@ function makeANiceEmail(text: string): string{
         ">
             <h2>Hello There!</h2>
             <p>${text}</p>
-            <p>[]:) PJ Toss</p>
+            <p> :) PJ Toss</p>
         </div>
     `;
 }
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d7c90e25d9587c153b3"; }
+/******/ 		__webpack_require__.h = function() { return "f5ae36cc964a184540b2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
