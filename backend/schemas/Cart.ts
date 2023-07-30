7864eafbf19f9a4c8f27051711fbe352e4dee96b
PJMantoss <joelptoss@gmail.com>
2023-07-30 19:50:00 +0100

    update Cart

--- backend/schemas/Cart.ts
+++ backend/schemas/Cart.ts
@@ -3,6 +3,11 @@ import { list } from '@keystone-next/keystone/schema';
 
 export const CartItem = list({
     fields: {
-        
+        quantity: integer({
+            defaultVaue: 1,
+            isRequired: true,
+        }),
+        product: relationship({ ref: 'Product' }),
+        user: relationship({ ref: 'User.cart' }),
     },
 });
