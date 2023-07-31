f9859e372d9c3adeffef0bba61250728e1d15916
PJMantoss <joelptoss@gmail.com>
2023-07-31 19:10:32 +0100

    update Header component

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "41bc5c98a870487c12ef"; }
+/******/ 		__webpack_require__.h = function() { return "7be009430d42e5658dc5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
