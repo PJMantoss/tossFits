b43fe588c3f103c797b421799c059bae33f5a011
PJMantoss <joelptoss@gmail.com>
2023-08-03 20:54:07 +0100

    update Nav

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d705099cc4769e376e5"; }
+/******/ 		__webpack_require__.h = function() { return "bcb3e947264871617407"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
+import SignOut from './SignOut';
 
 function Nav() {
   const user = useUser();
