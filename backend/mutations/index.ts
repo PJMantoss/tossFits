49dd90e9493ea1a5c2a5e4728349143aa604d81c
PJMantoss <joelptoss@gmail.com>
2023-08-05 19:24:24 +0100

    update mutation

--- backend/mutations/index.ts
+++ backend/mutations/index.ts
@@ -1,6 +1,6 @@
 import { graphQLSchemaExtension } from '@keystone-next/keystone/schema';
 
 export cont exportGraphQLSchema = graphQLSchemaExtension({
-    //typedefs
-    resolvers
-});
+    typedefs
+    //resolvers
+})
