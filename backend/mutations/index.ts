6b7d16702cc8e7bb947c743f1d5433395f762d61
PJMantoss <joelptoss@gmail.com>
2023-08-05 19:26:34 +0100

    update mutation

--- backend/mutations/index.ts
+++ backend/mutations/index.ts
@@ -3,7 +3,7 @@ import { graphQLSchemaExtension } from '@keystone-next/keystone/schema';
 export const exportGraphQLSchema = graphQLSchemaExtension({
     typeDefs:`
         type mutation {
-            
+            addToCart(productID)
         }
     `
     //resolvers
