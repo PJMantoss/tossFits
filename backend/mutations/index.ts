c27d558311a891c4a2fbd861da5bf4438af7ca90
PJMantoss <joelptoss@gmail.com>
2023-08-06 17:52:23 +0100

    update mutation

--- backend/mutations/index.ts
+++ backend/mutations/index.ts
@@ -3,11 +3,10 @@ import { graphQLSchemaExtension } from '@keystone-next/keystone/schema';
 // fake graphql tagged template literal string
 const graphql = String.raw;
 
-export const extendGraphQLSchema = graphQLSchemaExtension({
+export const extendGraphqlSchema = graphQLSchemaExtension({
     typeDefs: graphql`
         type Mutation {
             addToCart(productID: ID): CartItem
         }
     `
-    //resolvers
 });
