1ca481a6b830ac3c546a8eb8657c5b95ce509e33
PJMantoss <joelptoss@gmail.com>
2023-08-06 18:48:20 +0100

    update addToCart

--- backend/mutations/addToCart.ts
+++ backend/mutations/addToCart.ts
@@ -1,12 +1,15 @@
 import { KeystoneContext, SessionStore } from "@keystone-next/types";
 import { CartItemCreateInput } from '../.keystone/schema-types';
+import { Session } from '../type';
 
 async function addToCart(
     root: any, 
-    { productId }: { productId: String },
+    { productId }: { productId: string },
     context: KeystoneContext
-    ): Promise<CartItemCreateInput>{
+    ): Promise<CartItemCreateInput> {
         console.log('ADDING TO CART')
+        // query current user to see if they are signed in
+        const see = 
     };
 
 
